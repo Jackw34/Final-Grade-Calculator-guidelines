1bec2187947d107fa65826ab2c5f5c02f84204b3
Jack <Jwenschhof@bsd220.org>
2021-10-12 10:57:40 -0500

    Second Commit

---
+++ Final Grade Calculator guidelines/Final_Grade_Calculator_guidelinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Grade_Calculator_guidelinesApp.swift
+//  Final Grade Calculator guidelines
+//
+//  Created by Student on 9/22/21.
+//
+
+import SwiftUI
+
+@main
+struct Final_Grade_Calculator_guidelinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
